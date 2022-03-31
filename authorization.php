9bdacc8b87020e292f52fd3482c1e089b3d92c81
ellijahcuts <ellijahcuts@gmail.com>
2022-03-31 23:08:10 +0300

    Initial commit

---
+++ authorization.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<form class="loginForm" action="" method="">
+    <label>Логин</label>
+    <input type="text" placeholder="Введите имя пользователя">
+    <label>Пароль</label>
+    <input type="password" placeholder="Введите пароль">
+    <button>Войти</button>
+    <p>Вы не зарегистрированы?  -<a href="#">  Регистрация</a></p>
+</form>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<form class="loginForm" action="" method="">
+    <label>1. Имя</label>
+    <input type="text" placeholder="Введите ваше имя">
+    <label>2. Адрес электронной почты</label>
+    <input type="email" placeholder="Введите адрес электронной почты">
+    <label>3. Логин</label>
+    <input type="text" placeholder="Введите имя пользователя(никнейм)">
+    <label>4. Пароль</label>
+    <input type="password" placeholder="Введите пароль">
+    <label>5. Подтвердите пароль</label>
+    <input type="password" placeholder="Подтвердите пароль">
+    <label>6. Аватар профиля (необязательно) </label>
+    <input type="file">
+
+
+    <button>Регистрация</button>
+    <p>Вы уже зарегистрированы?  -  <a href="#">Авторизация</a></p>
+</form>
+</body>
+</html>
